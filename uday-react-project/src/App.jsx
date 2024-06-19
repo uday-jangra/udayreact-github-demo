7e7f5cf7a57bdbabc05a2cfb227fec44cfd14fa5
Uday <uday-jangra@talink.io>
2024-06-19 09:58:53 -0400

    Change

--- uday-react-project/src/App.jsx
+++ uday-react-project/src/App.jsx
@@ -22,9 +22,7 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs">UDAY C0897899</p>
     </>
   );
 }
